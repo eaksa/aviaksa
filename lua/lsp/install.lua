d04c9f68222f1967f266d83c5996fc3fcd038d3a
Elang Adhyaksa <elang@legica.ai>
2025-05-15 09:10:07 +0700

    feat: add lsp plugins

---
+++ lua/lsp/install.lua
@@ -0,0 +1,17 @@
+return {
+    servers = {
+        "bashls",			-- bash-language-server
+        "cssls",			-- css-lsp
+		"cypher_ls",		-- cypher-language-server
+		"docker_compose_language_service",
+		"dockerls",			-- dockerfile-language-server
+		"gopls",
+        "html",				-- html-lsp
+        "jsonls",			-- json-lsp
+        "lua_ls",			-- lua-language-server
+		"marksman",
+        "pyright",
+        "ts_ls",			-- typescript-language-server
+        "yamlls",			-- yaml-language-server
+    },
+}
---
+++ lua/plugins/lsp-mason.lua
@@ -0,0 +1,35 @@
+local M = {
+    "williamboman/mason.nvim",
+	version = "^2.0.0",
+    cmd = "Mason",
+    event = "BufReadPre",
+    dependencies = {
+        {
+            "williamboman/mason-lspconfig.nvim",
+            lazy = true,
+        },
+    },
+}
+
+local settings = {
+    ui = {
+        border = { "", "█", "", "█", "", "█", "", "█" },
+        icons = {
+            package_installed = "◍",
+            package_pending = "◍",
+            package_uninstalled = "◍",
+        },
+    },
+    log_level = vim.log.levels.INFO,
+    max_concurrent_installers = 4,
+}
+
+function M.config()
+    require("mason").setup(settings)
+    require("mason-lspconfig").setup({
+        ensure_installed = require("lsp.install").servers,
+        automatic_installation = true,
+    })
+end
+
+return M
