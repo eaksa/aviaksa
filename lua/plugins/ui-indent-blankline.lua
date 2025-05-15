a0f32c24b0ea97a41a8d7050efae1b98ab28921f
Elang Adhyaksa <elang@legica.ai>
2025-05-15 11:03:54 +0700

    fix: revert indent-blankline to version 2

--- lua/plugins/ui-indent-blankline.lua
+++ lua/plugins/ui-indent-blankline.lua
@@ -1,22 +1,21 @@
 local M = {
     "lukas-reineke/indent-blankline.nvim",
-	version = "^3.9.0",
+	version = "^2.20.8",
     event = "BufReadPre",
 }
 
--- TODO: Migrate from version 2 opts to version 3
--- M.opts = {
---     char = "∙",
---     show_trailing_blankline_indent = false,
---     show_first_indent_level = true,
---     use_treesitter = true,
---     show_current_context = true,
---     buftype_exclude = { "terminal", "nofile" },
---     filetype_exclude = {
---         "help",
---         "packer",
---         "NvimTree",
---     },
--- }
+M.opts = {
+    char = "∙",
+    show_trailing_blankline_indent = false,
+    show_first_indent_level = true,
+    use_treesitter = true,
+    show_current_context = true,
+    buftype_exclude = { "terminal", "nofile" },
+    filetype_exclude = {
+        "help",
+        "packer",
+        "NvimTree",
+    },
+}
 
 return M
